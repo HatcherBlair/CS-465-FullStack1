3ebbeb849178c885ea1d370d642643b3c6eb595d
HatcherBlair <hatcherbb@gmail.com>
2024-02-20 12:58:58 -0700

    Created user schema for DB

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,68 +1,73 @@
-const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1';
+const mongoose = require("mongoose");
+const host = process.env.DB_HOST || "127.0.0.1";
 const dbURI = `mongodb://${host}/travlr`;
-const readLine = require('readline');
+const readLine = require("readline");
 
 const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
+  setTimeout(
+    () =>
+      mongoose.connect(dbURI, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
-        useCreateIndex: true
-    }), 1000);
+        useCreateIndex: true,
+      }),
+    1000
+  );
 };
 
-mongoose.connection.on('connected', () => {
-    console.log('connected');
+mongoose.connection.on("connected", () => {
+  console.log("connected");
 });
 
-mongoose.connection.on('error', err => {
-    console.log('error:' + err);
-    return connect();
+mongoose.connection.on("error", (err) => {
+  console.log("error:" + err);
+  return connect();
 });
 
-mongoose.connection.on('dissconected', () => {
-    console.log('dissconnected');
+mongoose.connection.on("dissconected", () => {
+  console.log("dissconnected");
 });
 
-if (process.platform == 'win32') {
-    const rl = readLine.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.on('SIGINT', () => {
-        process.emit('SIGINT');
-    });
-};
+if (process.platform == "win32") {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.on("SIGINT", () => {
+    process.emit("SIGINT");
+  });
+}
 
 const gracefulShutdown = (msg, callback) => {
-    mongoose.connection.close(() => {
-        console.log(`Mongoose disconnected throuh ${msg}`);
-        callback();
-    });
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconnected throuh ${msg}`);
+    callback();
+  });
 };
 
 // For nodemon restarts
-process.once('SIGUSR2', () => {
-    gracefulShutdown('nodemon restart', () => {
-        process.kill(process.pid, 'SIGUSR2');
-    });
+process.once("SIGUSR2", () => {
+  gracefulShutdown("nodemon restart", () => {
+    process.kill(process.pid, "SIGUSR2");
+  });
 });
 
 // For app termination
-process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () =>{
-        process.exit(0);
-    });
+process.on("SIGINT", () => {
+  gracefulShutdown("app termination", () => {
+    process.exit(0);
+  });
 });
 
 // For Heroku app termination
-process.on('SIGTERM', () => {
-    gracefulShutdown('Heroku app shutdown', () => {
-        process.exit(0);
-    });
+process.on("SIGTERM", () => {
+  gracefulShutdown("Heroku app shutdown", () => {
+    process.exit(0);
+  });
 });
 
 connect();
 
 // Bring in the mongoose schema
-require('./travlr');
+require("./travlr");
+require("./user");
